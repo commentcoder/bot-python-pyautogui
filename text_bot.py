c9e93b20ab62a336e4319b72f4599cd84ab37fd3
Thomas Collart <thomas.collart@gmail.com>
2024-07-11 07:33:02 -0400

    Update text_bot.py

--- text_bot.py
+++ text_bot.py
@@ -2,6 +2,27 @@ import time
 import pyautogui
 
 time.sleep(2)
-  
+
+x = 809
+y_start = 415
+y_step = 457 - 415
+
+while True:
 im = pyautogui.screenshot(region=(675, 398, 772 - 675, 699 - 398))
-im.save('rose.png')
+
+lines = pytesseract.image_to_string(im)
+
+lines = res.strip() \
+    .replace('/20', '') \
+    .replace('/ 20', '') \
+    .replace('[20', '') \
+    .split('\n')
+
+is_over = True
+for i, line in enumerate(lines):
+    for _ in range(20 - int(line)):
+      pyautogui.click(810, 415 + step * counter)
+      is_over = False
+
+  if is_over == True:
+    quit()
